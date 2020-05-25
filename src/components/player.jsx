515b2df0f612782250efe9b5d06df22231fcd6ce
Micah Cantor <Micah_Cantor@Debras-Air.fios-router.home>
2020-05-25 14:14:45 -0400

    fixed pause icon

--- src/components/player.jsx
+++ src/components/player.jsx
@@ -3,7 +3,7 @@ import '../styles.css';
 
 const Player = () => {
     return (
-        <div className="bg-black">
+        <div className="bg-black h-20">
             <PlaybackControls> </PlaybackControls>
         </div>
     );
@@ -11,12 +11,19 @@ const Player = () => {
 
 const PlaybackControls = () => {
     return (
-        <div className="mx-auto">
-            <button className="rounded-full mx-auto h-16 w-16 flex items-center border-2 border-white" type="button">
-                <div className="ml-4 mr-1 rounded-full py-4 px-1 border-2 border-white"> </div>
-                <div className="mr-4 rounded-full py-4 px-1 border-2 border-white"> </div>
-            </button>
+        <div className="flex">
+            <PausePlay> </PausePlay>
         </div>
+    );
+};
+
+const PausePlay = () => {
+    return (
+        <button className="rounded-full mx-auto h-16 w-16 flex items-center mt-2" type="button">
+            <svg className="w-16 h-16 mx-auto" fill="none" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" stroke="white" viewBox="0 0 24 24">
+                <path d="M10 9v6m4-6v6m7-3a9 9 0 11-18 0 9 9 0 0118 0z"> </path>
+            </svg>
+        </button>
     );
 };
 
