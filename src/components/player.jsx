fbb441a281d0753727064ce7d07517019b39fbfc
Micah Cantor <Micah_Cantor@Debras-Air.fios-router.home>
2020-05-26 19:48:36 -0400

    fixed pause button alignment

--- src/components/player.jsx
+++ src/components/player.jsx
@@ -43,10 +43,20 @@ const ProgressBar = () => {
         </div>
     );
 };
-
+/*
+class ProgressBar extends React.Component {
+    render() {
+        return (
+            <div className="flex shadow w-full h-2 bg-grey-light">
+                <div className="bg-green text-xs leading-none py-1" style={{ width: '60%' }}> </div>
+            </div>
+        );
+    }
+}
+ */
 const PausePlay = () => {
     return (
-        <button className="flex rounded-full h-16 w-16 flex items-center mt-2 mx-auto" type="button">
+        <button className="rounded-full h-16 w-16 flex items-center mt-2" type="button">
             <svg className="flex w-16 h-16" fill="none" strokeLinecap="round" strokeLinejoin="round" strokeWidth=".75" stroke="white" viewBox="0 0 24 24">
                 <path d="M10 9v6m4-6v6m7-3a9 9 0 11-18 0 9 9 0 0118 0z"> </path>
             </svg>
@@ -56,8 +66,8 @@ const PausePlay = () => {
 
 const RightSkip = () => {
     return (
-        <button className="flex mt-2 mx-auto" type="button">
-            <svg className="flex w-12 h-12" fill="none" strokeLinecap="round" strokeLinejoin="round" strokeWidth=".75" stroke="white" viewBox="0 0 24 24">
+        <button className="mt-2" type="button">
+            <svg className="w-12 h-12" fill="none" strokeLinecap="round" strokeLinejoin="round" strokeWidth=".75" stroke="white" viewBox="0 0 24 24">
                 <path d="M9 5l7 7-7 7"> </path>
             </svg>
         </button>
@@ -66,8 +76,8 @@ const RightSkip = () => {
 
 const LeftSkip = () => {
     return (
-        <button className="flex mt-2 mx-auto" type="button">
-            <svg className="flex w-12 h-12" fill="none" strokeLinecap="round" strokeLinejoin="round" strokeWidth=".75" stroke="white" viewBox="0 0 24 24">
+        <button className="mt-2" type="button">
+            <svg className="w-12 h-12" fill="none" strokeLinecap="round" strokeLinejoin="round" strokeWidth=".75" stroke="white" viewBox="0 0 24 24">
                 <path d="M15 19l-7-7 7-7"> </path>
             </svg>
         </button>
