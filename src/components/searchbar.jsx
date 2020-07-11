abbe155bb9f003f36dbebcd4e4351d0851540610
Micah Cantor <micahcantor01@gmail.com>
2020-07-11 13:17:08 -0400

    started improving search bar

--- src/components/searchbar.jsx
+++ src/components/searchbar.jsx
@@ -1,13 +1,22 @@
-import React from "react";
+import React, { useState } from "react";
 import "../styles.css";
 
 const SearchBar = () => {
+  const [value, setValue] = useState("")
+
+  const handleChange = (e) => {
+    setValue(e.target.value)
+    console.log(value)
+    /* use value here, like send to server/search endpoint
+      then handle the results
+      then load them into a SearchResults component and display the component */
+  }
   return (
     <input
       className="transition-colors duration-200 ease-in-out bg-gray-200 appearance-none border-2 border-transparent rounded w-full mb-5 py-3 px-4 text-gray-700 leading-tight focus:outline-none hover:bg-white focus:border-green-400"
-      id=""
       type="text"
       placeholder="Song search"
+      onChange={handleChange}
     />
   );
 };
