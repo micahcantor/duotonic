744922ccb39765ac544e40acf429ee36e92c0ca3
Micah Cantor <micahcantor01@gmail.com>
2020-08-22 13:44:27 -0400

    small swap css change

--- app/src/components/swap.jsx
+++ app/src/components/swap.jsx
@@ -4,7 +4,7 @@ import "../styles.css";
 const SwapIcon = ({ onClick }) => {
     
     return (
-        <button onClick={onClick} type="button" className="md:hidden absolute inset-y-0 right-0 mt-2 mr-2 w-8 h-8">
+        <button onClick={onClick} type="button" className="md:hidden absolute inset-y-0 right-0 mt-2 mr-3 w-8 h-8">
             <svg className="stroke-current hover:text-customgreen" fill="none" strokeLinecap="round" strokeLinejoin="round" strokeWidth="1" viewBox="0 0 24 24">
                 <path d="M8 7h12m0 0l-4-4m4 4l-4 4m0 6H4m0 0l4 4m-4-4l4-4"></path>
             </svg>
