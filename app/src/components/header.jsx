36f8195c3049b434e3353d21068ab673274a9cff
Micah Cantor <micahcantor01@gmail.com>
2020-08-22 13:43:47 -0400

    dilate icons on hover

--- app/src/components/header.jsx
+++ app/src/components/header.jsx
@@ -103,7 +103,7 @@ const Header = ({ device, deviceSearching }) => {
 const NavButton = (props) => {
   return (
     <button onClick={props.open}>
-      <div className="inline-block bg-customgreen lowercase font-semibold px-3 py-2 leading-none rounded border-2 border-transparent hover:border-white mt-4 lg:mt-0">
+      <div className="inline-block bg-customgreen lowercase font-semibold px-3 py-2 leading-none rounded transform hover:scale-110 mt-4 lg:mt-0">
         {props.buttonText}
       </div>
     </button>
@@ -120,11 +120,11 @@ const HamburgerButton = (props) => {
   );
 }
 
-const ConnectedIndicator = ({ device }) => {
+const ConnectedIndicator = () => {
   return (
     <TriangleTooltip label="connected to spotify">
       <div className="flex items-center font-mono text-customgreen">
-        <svg className="fill-current w-6 h-6" viewBox="0 0 20 20">
+        <svg className="fill-current w-6 h-6 transform hover:scale-125" viewBox="0 0 20 20">
           <path fillRule="evenodd" d="M10 18a8 8 0 100-16 8 8 0 000 16zm3.707-9.293a1 1 0 00-1.414-1.414L9 10.586 7.707 9.293a1 1 0 00-1.414 1.414l2 2a1 1 0 001.414 0l4-4z" clipRule="evenodd"></path>
         </svg>
       </div>
@@ -136,7 +136,7 @@ const DisconnectedIndicator = () => {
   return (
     <TriangleTooltip label="spotify not connected">
       <div className="flex items-center text-red-700">
-        <svg className="fill-current w-6 h-6" viewBox="0 0 20 20">
+        <svg className="fill-current w-6 h-6 transform hover:scale-125" viewBox="0 0 20 20">
           <path fillRule="evenodd" d="M18 10a8 8 0 11-16 0 8 8 0 0116 0zm-7 4a1 1 0 11-2 0 1 1 0 012 0zm-1-9a1 1 0 00-1 1v4a1 1 0 102 0V6a1 1 0 00-1-1z" clipRule="evenodd"></path>
         </svg>
       </div>
@@ -148,7 +148,7 @@ const UserAloneIndicator = () => {
   return (
     <TriangleTooltip label="you're all alone">
       <div className="flex items-center -mt-1">
-        <svg className="fill-current w-6 h-6" viewBox="0 0 20 20" >
+        <svg className="fill-current w-6 h-6 transform hover:scale-125" viewBox="0 0 20 20" >
           <path fillRule="evenodd" d="M10 9a3 3 0 100-6 3 3 0 000 6zm-7 9a7 7 0 1114 0H3z" clipRule="evenodd"></path>
         </svg>
       </div>
@@ -165,7 +165,7 @@ const LeaveRoomButton = () => {
   return (
     <TriangleTooltip label="leave room" > 
       <button className="flex items-center text-red-700" onClick={leaveRoom}>
-        <svg className="fill-current w-6 h-6" viewBox="0 0 20 20" >
+        <svg className="fill-current w-6 h-6 transform hover:scale-125" viewBox="0 0 20 20" >
           <path fillRule="evenodd" d="M3 3a1 1 0 00-1 1v12a1 1 0 102 0V4a1 1 0 00-1-1zm10.293 9.293a1 1 0 001.414 1.414l3-3a1 1 0 000-1.414l-3-3a1 1 0 10-1.414 1.414L14.586 9H7a1 1 0 100 2h7.586l-1.293 1.293z" clipRule="evenodd"></path>
         </svg>
       </button>
