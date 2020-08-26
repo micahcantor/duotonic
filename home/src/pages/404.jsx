18106d3e6db84398e667a89fa83d490cd481f026
Micah Cantor <micahcantor01@gmail.com>
2020-08-25 22:05:03 -0400

    add footer

---
+++ home/src/pages/404.jsx
@@ -0,0 +1,13 @@
+import React from "react"
+import Header from "../components/header.jsx"
+
+export default () => {
+    return (
+        <div className="h-screen bg-bgDark text-textColor flex flex-col">
+            <div className="container w-full h-full mx-auto pb-16">
+                <Header showButton={false}/>
+                <span className="text-6xl font-black text-primary mx-auto"> Page not found </span>
+            </div>
+        </div>
+    );
+}
