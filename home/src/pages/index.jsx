b32447c8277476bd6ba485b8f76736ba3d624110
Micah Cantor <micahcantor01@gmail.com>
2020-08-25 22:51:18 -0400

    add seo component

--- home/src/pages/index.jsx
+++ home/src/pages/index.jsx
@@ -1,24 +1,28 @@
 import React from "react"
 import { graphql } from "gatsby"
 
+import SEO from "../components/seo.js"
 import Header from "../components/header.jsx"
 import Footer from "../components/footer.jsx"
 import { Hero, Sync, Chat, Pairing } from "../components/sections.jsx"
 
 const IndexPage = ({ data }) => {
   return (
-    <div className="bg-bgDark text-textColor flex flex-col">
-      <div className="container w-full h-full mx-auto pb-16">
-        <Header showButton={true}/>
-        <main className="flex flex-col flex-grow items-center w-full divide-textColor divide-y-8 space-y-8 pb-8 overflow-hidden">
-          <Hero data={data}/>
-          <Sync data={data}/>
-          <Chat data={data}/>
-          <Pairing data={data}/>
-        </main>
-        <Footer />
+    <>
+      <SEO title="Duotonic" />
+      <div className="bg-bgDark text-textColor flex flex-col">
+        <div className="container w-full h-full mx-auto pb-16">
+          <Header showButton={true}/>
+          <main className="flex flex-col flex-grow items-center w-full divide-textColor divide-y-8 space-y-8 pb-8 overflow-hidden">
+            <Hero data={data}/>
+            <Sync data={data}/>
+            <Chat data={data}/>
+            <Pairing data={data}/>
+          </main>
+          <Footer />
+        </div>
       </div>
-    </div>
+    </>
   )
 }
 
