bb67b170b656036d0889d5379a348fc2d8debe88
Micah Cantor <micahcantor01@gmail.com>
2020-08-30 00:20:04 -0400

    refactor playback init

--- app_gatsby/src/web_playback.js
+++ app_gatsby/src/web_playback.js
@@ -1,10 +1,7 @@
 import { getAccessToken } from "./api.js";
 import Bowser from "bowser";
 
-var insertPlayerInterval;
 var device;
-
-export var player;
 
 export const addSDKScript = () => {
   const webSDK = document.createElement("script");
@@ -49,65 +46,58 @@ export const isPlaybackCapable = () => {
   return supported
 };
 
-const createEventHandlers = () => {
+const createErrorHandlers = (player) => {
   player.on("initialization_error", (e) => {
-    console.error(e);
+    console.log(e);
   });
   player.on("authentication_error", (e) => {
-    console.error(e);
+    console.log(e);
   });
   player.on("account_error", (e) => {
-    console.error(e);
+    console.log(e);
   });
   player.on("playback_error", (e) => {
-    console.error(e);
-  });
-
-  // Playback status updates
-  player.on("player_state_changed", (state) => {
-    //console.log(state);
-  });
-
-  // Ready
-  player.addListener("ready", ({ device_id }) => {
-    device = {
-      id: device_id,
-      name: "Browser Playback"
-    }
+    console.log(e);
   });
 };
 
-const insertPlayer = async () => {
-  if (window.Spotify != null) {
-    clearInterval(insertPlayerInterval); // clear the interval once found
-
-    const token = await getAccessToken();
-    player = new window.Spotify.Player({
-      name: "Duotonic",
-      /* this looks weird but is just boilerplate for initializing the player */
-      getOAuthToken: (callback) => {
-        callback(token);
-      },
+const getDevice = (player) => {
+  return new Promise(resolve => {
+    player.addListener("ready", ({ device_id }) => {
+      device = {
+        id: device_id,
+        name: "Browser Playback"
+      }
+      resolve(device);
     });
+  })
+}
 
-    createEventHandlers();
-    player.connect();
-  }
+const insertPlayer = () => {
+  const player = new window.Spotify.Player({
+    name: "Duotonic",
+    /* this function is run when player.connect() is called */
+    getOAuthToken: function(callback) {
+      getAccessToken().then(token => callback(token))
+    },
+  });
+
+  return player;``
 };
 
 export const initPlayer = async () => {
   return new Promise((resolve) => {
-    insertPlayerInterval = setInterval(() => {
-      insertPlayer();
+    const insertPlayerInterval = setInterval(async () => {
+      if (window.Spotify) {
+        const player = await insertPlayer();
+        const device = await getDevice(player);
+        const playerData = { device: device, player: player }
 
-      const getDeviceIDInterval = setInterval(() => {
-        if (device) {
-          const playerData = { device: device, player: player }
-          resolve(playerData);
-          clearInterval(getDeviceIDInterval);
-        }
-      }, 250);
-
+        player.connect();
+        createErrorHandlers(player);
+        clearInterval(insertPlayerInterval);
+        resolve(playerData);
+      }
     }, 250);
   });
 };
