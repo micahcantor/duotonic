eebbb98b28f54202e81d8483a56a4977906f2df1
Micah Cantor <micahcantor01@gmail.com>
2020-08-30 15:04:26 -0400

    fix eslint useffect depency issue

--- app_gatsby/src/pages/index.jsx
+++ app_gatsby/src/pages/index.jsx
@@ -62,8 +62,17 @@ const App = () => {
     setRoom(room);
   }, []);
 
+  /* Fires after the sign in link has been set with the query params it needs (room ID and playback capability) */
+  useEffect(() => {
+    if (signInLink) {
+      setModalBody(modals.SignIn);
+      setShowModal(true);
+    }
+  }, [signInLink]);
+
   /* Fires after the user is authorized, connected to a device, and in a room */
   useEffect(() => {
+
     if (isAuthorized && device && room) {
       initRoomSocket().then(async (msg) => {
         if (msg !== "error") {
@@ -72,15 +81,87 @@ const App = () => {
         }
       });
     }
-  }, [isAuthorized, device, room]);
 
-  /* Fires after the sign in link has been set with the query params it needs (room ID and playback capability) */
-  useEffect(() => {
-    if (signInLink) {
-      setModalBody(modals.SignIn);
-      setShowModal(true);
+    /* Functions needed to do this effect included here in the callback body */
+    async function initRoomSocket() {
+      console.log('entering room', room, 'with device ', device.id);
+      const { msg } = await enterRoom(room);
+      if (msg === "error") {
+        setShowModal(true);
+        setModalBody(modals.RoomNotFound);
+        window.history.pushState(null, null, "/");
+      }
+      else {
+        console.log("connecting to websocket in room", room)
+        const client = new Nes.Client("ws://localhost:3000");
+        await client.connect();
+        client.subscribe(`/rooms/playback/${room}`, handleRoomPlaybackUpdate);
+        setWSClient(client);
+      }
+  
+      return msg;
     }
-  }, [signInLink]);
+
+    async function loadPlaybackLocally(playback) {
+      const {isPaused, position_ms, queue, history, current_song} = playback;
+      //queue.forEach(async (song) => await addToQueue(device.id, song, room, false));
+  
+      if (current_song && !isPaused && Object.keys(current_song).length !== 0) {
+        await startSong(device.id, current_song, room, false);
+        await setSongPosition(device.id, position_ms, room, false);
+        setSeekUpdateElapsed(position_ms);
+      }
+  
+      setIsPaused(isPaused);
+      setHistory([...history]);
+      updateSongs([...queue]);
+    }
+
+    async function handleRoomPlaybackUpdate({ updated, type }) {
+      console.log(updated, type);
+      switch(type) {
+        case 'start':
+          console.log('received update to start')
+          await startSong(device.id, updated.current_song, room, false);
+          updateSongs(songs => songs.concat(updated.current_song));
+          setIsPaused(false);
+          break;
+        case 'resume':
+          await resumeSong(device.id, room, false);
+          setIsPaused(false);
+          break;
+        case 'pause':
+          await pauseSong(device.id, room, false);
+          setIsPaused(true);
+          break;
+        case 'next':
+          console.log('next song from room update')
+          await nextSong(device.id, updated.current_song, room, false);
+          updateSongs(songs => songs.filter((s, i) => i > 0));
+          setIsPaused(false);
+          break;
+        case 'previous':
+          await previousSong(device.id, updated.current_song, room, false);
+          updateSongs(songs => {
+            const updated = [...songs];
+            updated[0] = history[history.length - 1];
+            return updated;
+          });
+          setIsPaused(false);
+          break;
+        case 'queue':
+          await addToQueue(device.id, updated.current_song, room, false);
+          updateSongs(songs => songs.concat(updated.current_song));
+          break;
+        case 'seek':
+          const position = parseInt(updated.position_ms);
+          await setSongPosition(device.id, position, room, false);
+          setSeekUpdateElapsed(position / 1000);
+          break;
+        default: break;
+      }
+    }
+  }, [isAuthorized, device, room, history]);
 
   const setupWebPlayer = async () => {
     addSDKScript();
@@ -106,40 +187,6 @@ const App = () => {
     }, 1000)
   }
 
-  const initRoomSocket = async () => {
-    /* separate function for this since useEffect callback can't be async */
-    console.log('entering room', room, 'with device ', device.id);
-    const { msg } = await enterRoom(room);
-    if (msg === "error") {
-      setShowModal(true);
-      setModalBody(modals.RoomNotFound);
-      window.history.pushState(null, null, "/");
-    }
-    else {
-      console.log("connecting to websocket in room", room)
-      const client = new Nes.Client("ws://localhost:3000");
-      await client.connect();
-      client.subscribe(`/rooms/playback/${room}`, handleRoomPlaybackUpdate);
-      setWSClient(client);
-    }
-
-    return msg;
-  }
-
-  const loadPlaybackLocally = async (playback) => {
-    const {isPaused, position_ms, queue, history, current_song} = playback;
-    queue.forEach(async (song) => await addToQueue(device.id, song, room, false));
-    if (current_song && Object.keys(current_song).length !== 0) {
-      await startSong(device.id, current_song, room, false);
-      await setSongPosition(device.id, position_ms, room, false);
-      setSeekUpdateElapsed(position_ms);
-    }
-
-    setIsPaused(isPaused);
-    setHistory([...history]);
-    updateSongs([...queue]);
-  }
-
   const startRefreshTimer = () => {
     // refresh the access token roughly every hour if the user has not left the page
     const almost_one_hour = 55 * 60000; // 55 mins
@@ -150,51 +197,6 @@ const App = () => {
 
   /* Function that fires whenever the user receives a web socket message
     Updates the local state and the user's spotify playback with relevant info */
-  const handleRoomPlaybackUpdate = async ({ updated, type }) => {
-    console.log(updated, type);
-    switch(type) {
-      case 'start':
-        console.log('received update to start')
-        await startSong(device.id, updated.current_song, room, false);
-        updateSongs(songs => songs.concat(updated.current_song));
-        setIsPaused(false);
-        break;
-      case 'resume':
-        await resumeSong(device.id, room, false);
-        setIsPaused(false);
-        break;
-      case 'pause':
-        await pauseSong(device.id, room, false);
-        setIsPaused(true);
-        break;
-      case 'next':
-        console.log('next song from room update')
-        await nextSong(device.id, updated.current_song, room, false);
-        updateSongs(songs => songs.filter((s, i) => i > 0));
-        setIsPaused(false);
-        break;
-      case 'previous':
-        await previousSong(device.id, updated.current_song, room, false);
-        updateSongs(songs => {
-          const updated = [...songs];
-          updated[0] = history[history.length - 1];
-          return updated;
-        });
-        setIsPaused(false);
-        break;
-      case 'queue':
-        await addToQueue(device.id, updated.current_song, room, false);
-        updateSongs(songs => songs.concat(updated.current_song));
-        break;
-      case 'seek':
-        const position = parseInt(updated.position_ms);
-        await setSongPosition(device.id, position, room, false);
-        setSeekUpdateElapsed(position / 1000);
-        break;
-      default: break;
-    }
-  }
-
   return (
     <>
       <SEO title="App" />
